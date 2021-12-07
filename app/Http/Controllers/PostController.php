9e6c1c3007c91de7196f7d3435c2313270718425
yutaro101 <yutaro10120424@gmail.com>
2021-12-07 16:55:50 +0900

    環境設定　基礎実装　ルーティング

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    
+    public function index(Post $post)
+    {
+       return $post->get();
+    }
 }
